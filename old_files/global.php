eec831f26e6d8dd92589567db7e6c6ad08a39170
the0bob <bob@bobpalmer.info>
2018-07-16 20:48:25 -0500

    remove unneeded php files

--- old_files/global.php
+++
@@ -1,9 +0,0 @@
-<?php
-  /*
-  I'm going to dynamically generate a little content to make the site more search engine friendly. 
-  */
-  
-  //Figure out the next serving dates. This will keep the site current for search engines.
-  $nextDate = date('M d, Y',strtotime( "next sunday" )); //yes, it's really that easy
-  $nextDateDowntown = date('M d, Y',strtotime( "next friday" )); 
-?>
